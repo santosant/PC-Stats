afc9c99ddde6b254e90bb0bad7b2b513c9f1873c
santosant <hiroto.anderson@gmail.com>
2021-02-03 23:53:29 -0300

    end stats

--- stats.js
+++ stats.js
@@ -6,12 +6,12 @@ setInterval(() => {
 
     const total = parseInt(totalmem() / 1024 / 1024)
     const mem = parseInt(freemem() / 1024 / 1024)
-    const percents = parseInt(mem / total * 100)
+    const percent = parseInt(mem / total * 100)
 
     const stats = {
         free: `${mem} MB`,
         total: `${total}, MB`,
-        usage: `${percents}%`
+        usage: `${percent}%`
     }
 
     console.clear()
